e308310f0e0dba8fd25325135657ed01a2875743
Shiren Teji <shiren.teji28@gmail.com>
2024-01-19 21:09:54 +1100

    added question

---
+++ RookieDriver/Models/Question.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace RookieDriver.Models
+{
+    public class Question
+    {
+        public string Description { get; set; }
+        public string OptionA { get; set; }
+        public string OptionB { get; set; }
+        public string OptionC { get; set; }
+        public string OptionD { get; set; }
+        public string CorrectAnswer { get; set; }
+       
+
+        public bool IsAnswerCorrect (string UserAnswer)
+        { 
+            return CorrectAnswer == UserAnswer;
+        }
+
+
+    }
+}
