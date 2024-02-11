ea76de73f1e56b48388d0f9048c0ef76289405a5
yogeshck <yogesh.jun5@gmail.com>
2024-02-11 19:32:59 +1100

    Tutorials button

--- RookieDriver/RookieDriverContext.cs
+++ RookieDriver/RookieDriverContext.cs
@@ -5,5 +5,9 @@
         public static string LoggedInUsername { get; set; }
 
         public static bool IsUserLoggedIn { get; set; }
+
+        public static bool IsTestStarted { get; set; }
+
+        public static bool InTutorial { get; set; }
     }
 }
